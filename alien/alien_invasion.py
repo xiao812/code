955edd3ff7e5b53c355868fa22fac2e688e49d7f
小葱哥 <wuxiaocong93@gmail.com>
2020-04-11 22:02:45 +0800

    python小飞机
    python从入门到实践的例子

---
+++ alien/alien_invasion.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 21:57:30 2020
+
+@author: w
+"""
+
+import sys 
+import pygame 
+from settings import Settings 
+from ship import Ship 
+import game_functions as gf
+from pygame.sprite import Group
+
+def run_game():
+    # 初始化pygame, 设置和屏幕对象
+    pygame.init()
+    ai_settings= Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    
+    # 创建一艘飞船 
+    ship = Ship(ai_settings, screen)
+    
+    # 创建一个用于存储子弹的编组 
+    bullets = Group()
+    
+    
+    # 开始游戏的主循环
+    while True: 
+        # 监视键盘和鼠标时间
+        gf.check_events(ai_settings, screen, ship,bullets)
+        ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
+        
+run_game()
