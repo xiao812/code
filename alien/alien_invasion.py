39e70494c2e914009f092ea148f120c828407b1d
小葱哥 <wuxiaocong93@gmail.com>
2020-04-12 22:54:18 +0800

    打小飞机
    打小飞机

--- alien/alien_invasion.py
+++ alien/alien_invasion.py
@@ -11,6 +11,7 @@ from settings import Settings
 from ship import Ship 
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats 
 
 def run_game():
     # 初始化pygame, 设置和屏幕对象
@@ -25,13 +26,22 @@ def run_game():
     # 创建一个用于存储子弹的编组 
     bullets = Group()
     
+    aliens = Group()
+    
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship,  aliens)
+    
+    # 创建一个用于存储游戏统计信息的实例 
+    stats = GameStats(ai_settings)
     
     # 开始游戏的主循环
     while True: 
         # 监视键盘和鼠标时间
         gf.check_events(ai_settings, screen, ship,bullets)
-        ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()  
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens,  bullets)
         
 run_game()
--- alien/settings.py
+++ alien/settings.py
@@ -16,10 +16,18 @@ class Settings():
         
         # 飞船的设置 
         self.ship_speed_factor = 1.5 
-        
+        self.ship_limit = 3 
         
         # 子弹设置 
         self.bullet_speed_factor = 1 
         self.bullet_width = 3 
         self.bullet_height = 15 
         self.bullet_color = 60, 60, 60 
+        self.bullets_allowed = 10 
+        
+        # 外星人设置 
+        self.alien_speed_factor = 1 
+        self.alien_drop_speed = 30
+        # fleet_direction 为1表示右移动，为-1表示左移
+        self.fleet_direction = 1 
+        
